8a6fb193569f4cfc9b6363edcc86d30147c69037
alexandrossilva <alexandrossilva@gmail.com>
2024-10-23 19:32:04 -0300

    Aula do dia 23/10/2024

--- aulas/aula-07 (23-10-2024)/src/aula07/Listagem.java
+++ aulas/aula-07 (23-10-2024)/src/aula07/Listagem.java
@@ -9,9 +9,18 @@ public class Listagem {
         System.out.println("}");
     }
 
+    public static void listarArray(double[] array) {
+        System.out.print("{ ");
+        for (int i = 0; i < array.length; i++)
+            System.out.print(array[i] + " ");
+        System.out.println("}");
+    }
+
     public static void main(String[] args) {
         int[] array1 = new int[] {7, 2, 9, 1, 10};
         listarArray(array1);
+        double[] array2 = new int[] {7.2, 2.7, 9.1, 1.9, 10.7};
+        listarArray(array2);
     }
 
 }
